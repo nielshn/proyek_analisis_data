881b3b424a8f1936f3c3069b4e85e0deed1fc366
nielshn <niel.shn08@gmail.com>
2025-02-26 21:08:23 +0700

    update location dataset

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -4,7 +4,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # Load dataset
-day_df = pd.read_csv("../data/day.csv", parse_dates=['dteday'])
+day_df = pd.read_csv("data/day.csv", parse_dates=['dteday'])
 
 # Mapping season and weathersit to readable labels
 season_mapping = {1: "Spring", 2: "Summer", 3: "Fall", 4: "Winter"}
